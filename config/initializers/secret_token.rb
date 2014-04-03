297e9de1470b29defaccc9f253caeac08e557840
Juan Ortiz <juandefelix@yahoo.es>
2014-04-02 19:56:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsMakeupScheduler::Application.config.secret_key_base = '226315a26becbfdfefbed64b1afcd1bfe5c2bffd105559a412062137164730f7919ad51a184ba05e35e9b0dfec8c5d8f10cfbd1c916a936cec84f77a2b1ca44a'
