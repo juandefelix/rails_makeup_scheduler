619e7cc013c0f63e157274e854a1ddace6e3aeab
Juan Ortiz <juandefelix@yahoo.es>
2014-04-02 20:26:54 -0500

    added cancellation resources

---
+++ app/controllers/cancellations_controller.rb
@@ -0,0 +1,10 @@
+class CancellationsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+  
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RailsMakeupScheduler::Application.routes.draw do
+
+  resources :cancellations, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
