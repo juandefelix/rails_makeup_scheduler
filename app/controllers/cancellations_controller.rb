7ea3b3534a8f1cb93d0e4448ee276743ec9c6f25
Juan Ortiz <juandefelix@yahoo.es>
2014-04-04 19:00:46 -0500

    added form for creating a canellation

--- app/controllers/cancellations_controller.rb
+++ app/controllers/cancellations_controller.rb
@@ -1,9 +1,11 @@
 class CancellationsController < ApplicationController
 
   def new
+    @cancellation = Cancellation.new
   end
 
   def create
+    redirect_to @cancellation
   end
   
   def destroy
