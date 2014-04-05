35093b045e9faf49163476d65b1aa103ffe8f086
Juan Ortiz <juandefelix@yahoo.es>
2014-04-05 13:30:38 -0500

    adding validation and format tests for the cancellation model

--- spec/models/cancellation_spec.rb
+++ spec/models/cancellation_spec.rb
@@ -4,6 +4,9 @@ describe Cancellation do
   before { @cancellation = Cancellation.new(name: "Luis Solares", instrument:"guitar", date:"12-3-12", start_time:"12:00pm", end_time:"12:30pm") }
 
   subject { @cancellation }
+
+# respond_to attributes ===============================
+
   it { should respond_to(:name) }
   it { should respond_to(:instrument) }
   it { should respond_to(:date) }
@@ -11,6 +14,8 @@ describe Cancellation do
   it { should respond_to(:end_time) }
 
   it { should be_valid }
+
+# presence of attributes ===============================
 
   describe "when name is not present" do
     before { @cancellation.name = ""}
@@ -35,5 +40,43 @@ describe Cancellation do
   describe "when end_time is not present" do
     before { @cancellation.end_time = "" }
     it { should_not be_valid }
+  end
+
+# format of some attributes ==========================
+
+  describe "when using invalid formats for time" do
+    it "start_time should be invalid" do
+      wrong_times = %w[123pm 0123pm 01:23]
+      wrong_times.each do |wrong_time|
+        @cancellation.start_time = wrong_time
+        expect(@cancellation).not_to be_valid
+      end
+    end
+
+    it "end_time should be invalid" do
+      wrong_times = %w[123pm 0123pm 01:23]
+      wrong_times.each do |wrong_time|
+        @cancellation.end_time = wrong_time
+        expect(@cancellation).not_to be_valid
+      end # end do
+    end
+  end
+
+  describe "when using valid formats for time" do
+    it "start_time should be valid" do
+      valid_times = ['1:23 pm', '02:34 am', '01:23pm', '1:23am', '01:23  pm', '1:23  am']
+      valid_times.each do |valid_time|
+        @cancellation.start_time = valid_time
+        expect(@cancellation).to be_valid
+      end
+    end
+
+    it "end_time should be valid" do
+      valid_times = ['1:23 pm', '02:34 am', '01:23pm', '1:23am', '01:23  pm', '1:23  am']
+      valid_times.each do |valid_time|
+        @cancellation.end_time = valid_time
+        expect(@cancellation).to be_valid
+      end
+    end
   end
 end
