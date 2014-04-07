a06164cb3a09823a2f073de9de99c5d2baf2429f
Juan Ortiz <juandefelix@yahoo.es>
2014-04-07 13:36:19 -0500

    before refactoring custom.css.scss

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(:default, Rails.env)
 module RailsMakeupScheduler
   class Application < Rails::Application
 
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,5 +20,6 @@ module RailsMakeupScheduler
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
