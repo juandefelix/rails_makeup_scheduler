ac2206c86dbb4491fccf3ea759485d7aefbf8169
Juan Ortiz <juandefelix@yahoo.es>
2014-04-07 14:14:12 -0500

    refactored custom.css.scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 RailsMakeupScheduler::Application.routes.draw do
 
-  root 'cancellations#new'
+  root 'static_pages#home'
+  
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+
 
   get "static_pages/home"
   get "static_pages/help"
