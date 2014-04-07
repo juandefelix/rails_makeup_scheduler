e02471e0e398b3c663cf7f211281409065c6fe7a
Juan Ortiz <juandefelix@yahoo.es>
2014-04-07 11:50:56 -0500

    finished some basic static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ RailsMakeupScheduler::Application.routes.draw do
 
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   
   resources :cancellations
 
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home Page" do
+
+    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "should have the content 'Makeup Scheduler'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Makeup Scheduler')
+    end  
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Makeup Scheduler | Home')
+    end
+  end
+
+  describe "Help Page" do
+
+    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end  
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Makeup Scheduler | Help')
+    end
+  end
+
+  describe "About" do
+
+    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "should have the content 'Help'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end  
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Makeup Scheduler | About')
+    end
+  end
+end
