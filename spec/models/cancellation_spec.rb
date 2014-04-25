ae5ea879793ea2c17d2b3a6b7c5b61b2a3268422
Juan Ortiz <juandefelix@yahoo.es>
2014-04-25 08:47:23 -0500

    removed unnecessary tests

--- spec/models/cancellation_spec.rb
+++ spec/models/cancellation_spec.rb
@@ -37,22 +37,14 @@ describe Cancellation do
     it { should_not be_valid }
   end
 
-  # describe "when end_time is not present" do
-  #   before { @cancellation.end_time = "" }
-  #   it { should_not be_valid }
-  # end
 
 # format of some attributes ==========================
 
   describe "when using invalid format for time_start and time_end" do
     wrong_times = %w[123pm 0123pm 01:23]
     wrong_times.each do |wrong_time|
-
       before { @cancellation.start_time = wrong_time }
       it { should_not be_valid }
-
-      # before { @cancellation.end_time = wrong_time }
-      # it { should_not be_valid }
     end
   end
 
@@ -60,24 +52,8 @@ describe Cancellation do
   describe "when using valid formats for time_start and time_end" do
     valid_times = ['1:23 pm', '02:34 am', '01:23pm', '1:23am', '01:23  pm', '1:23  am']
     valid_times.each do |valid_time|
-
-        before { @cancellation.start_time = valid_time }
-        it { should be_valid}
-
-        # before { @cancellation.end_time = valid_time }
-        # it { should be_valid }
+      before { @cancellation.start_time = valid_time }
+      it { should be_valid}
     end
   end
 end
-
-=begin
-  
-define the array of wrong adresses
-
-for each one
-  assign the start_time
-  test @cancellation
-  assing the end_time
-  test @cancellation
-  
-=end
