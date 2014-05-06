15644c7b5509b0f7140c38e98e49d49df31290f3
Juan Ortiz <juandefelix@yahoo.es>
2014-05-06 08:44:18 -0500

    Make a basic Business model (including secure passwords)

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -10,6 +10,7 @@ describe Business do
   it { should respond_to :password_digest }
   it { should respond_to :password }
   it { should respond_to :password_confirmation }
+  it { should respond_to :authenticate }
 
   it { should be_valid }
 
@@ -33,7 +34,7 @@ describe Business do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+      addresses = %w[business@foo,com business_at_foo.org example.business@foo.
                      foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @business.email = invalid_address
@@ -44,7 +45,7 @@ describe Business do
 
   describe "when email format is valid" do
     it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses = %w[business@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
         @business.email = valid_address
         expect(@business).to be_valid
@@ -73,5 +74,26 @@ describe Business do
   describe "when password doesn't match confirmation" do
     before { @business.password_confirmation = "mismatch" }
     it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @business.password = @business.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @business.save }
+    let(:found_business) { Business.find_by email:(@business.email) }
+
+    describe "with valid password" do
+      it { should eq found_business.authenticate(@business.password) }
+    end
+
+    describe "with invalid password" do
+      let(:business_for_invalid_password) { found_business.authenticate("invalid") }
+
+      it { should_not eq business_for_invalid_password }
+      specify { expect(business_for_invalid_password).to be_false }
+    end
   end
 end
