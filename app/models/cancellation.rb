25d3a115acb80cc5bd81c63c2b3964cc3ef5b1f0
Juan Ortiz <juandefelix@yahoo.es>
2014-05-06 13:09:22 -0500

    more conditional validations

--- app/models/cancellation.rb
+++ app/models/cancellation.rb
@@ -1,19 +1,15 @@
 require 'pry'
 class Cancellation < ActiveRecord::Base
   validates :name, :instrument, :date, :start_time, presence: true
-  validates :start_time, format: { with: /[0-1][0-9]:\d{2}/i, message: "must follow this format: '(H)H:MM pm' e.g. '01:30 pm, 2:30pm'" }
-  validates :date, format: { with: /\A[012]?[0-9][-\/][0123]?[0-9][-\/](20)?[0-9]{2}\z/, message: "Invalid date format" }
+  validates :start_time, format: { with: /[0-1][0-9]:\d{2}/i, message: "must follow this 
+                                  format: '(H)H:MM pm' e.g. '01:30 pm, 2:30pm'" }, unless: 'start_time.blank?'
+  validates :date, format: { with: /\A[012]?[0-9][-\/][0123]?[0-9][-\/](20)?[0-9]{2}\z/, 
+                             message: "Invalid date format" }, unless: 'date.blank?'
+
+  # if they date and time have valid format, apply the other two validations 
   with_options if: :date_time_valid_format? do |cancellation|
-    cancellation.validate :cannot_be_in_the_past
+    cancellation.validate :in_the_past
     cancellation.validate :less_than_24
-  end
-
-  
-
-  def date_time_valid_format?
-    validate_date = self.date =~ /\A[012]?[0-9][-\/][0123]?[0-9][-\/](20)?[0-9]{2}\z/
-    validate_start_time = self.start_time =~ /[0-1][0-9]:\d{2}/i
-    validate_date && validate_start_time
   end
 
   def date_and_time
@@ -29,7 +25,13 @@ class Cancellation < ActiveRecord::Base
     Time.new(year, month, day, hour, minute)
   end
 
-  def cannot_be_in_the_past
+  def date_time_valid_format?
+    validate_date = self.date =~ /\A[012]?[0-9][-\/][0123]?[0-9][-\/](20)?[0-9]{2}\z/
+    validate_start_time = self.start_time =~ /[0-1][0-9]:\d{2}/i
+    validate_date && validate_start_time
+  end
+
+  def in_the_past
     errors[:base] << "The date and time can not be in the past" if date_and_time < Time.now
   end
 
