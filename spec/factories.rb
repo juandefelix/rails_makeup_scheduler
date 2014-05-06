96ef933b9c79f0efce5294e4a087c70c88831270
Juan Ortiz <juandefelix@yahoo.es>
2014-05-06 15:10:41 -0500

    corrected the tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :cancellation do
     name "Roberto Ruiz"
     instrument "Guitar"
-    date future_date
+    date "#{(Time.now + 2.day).strftime("%m/%d/%y")}"
     start_time "20:30"
   end
 end
