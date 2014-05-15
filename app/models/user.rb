efdd794112611396b793536994911868f7799d08
Juan Ortiz <juandefelix@yahoo.es>
2014-05-15 14:04:39 -0500

    added before_save and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   validates :name, :email, presence: true
   validates :name, length: { in: 4..50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
 
   has_secure_password
+
+  before_save { email.downcase! }
 end
 
