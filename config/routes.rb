7d3695757c3e9cea36505c375c4102403b26334c
Juan Ortiz <juandefelix@yahoo.es>
2014-05-15 15:30:34 -0500

    basic show template

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 RailsMakeupScheduler::Application.routes.draw do
 
-  get "users/new"
+  root 'static_pages#home'
+
+  match '/signup', to: 'users#new', via: 'get'
+  
   match '/cancellations(/:year(/:month))', to: 'cancellations#index', via: 'get', :as => :cancellations, 
                                           :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
-  root 'static_pages#home'
   
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
 
   
   resources :cancellations
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/factories.rb
+++ spec/factories.rb
@@ -5,5 +5,12 @@ FactoryGirl.define do
     start_at "#{(Time.now + 1.day + 3.hours).strftime("%Y-%m-%d %H:%M")}"
     end_at "#{(Time.now + 1.day + 3.hours + 30.minutes).strftime("%Y-%m-%d %H:%M")}"
   end
+
+  factory :user do
+    name                  "Example User"
+    email                 "user@example.com"
+    password              "foobar"
+    password_confirmation "foobar"
+  end
 end
 
