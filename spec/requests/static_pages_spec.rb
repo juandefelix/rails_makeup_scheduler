503ece0107d0f64381a9ef8c67573e5b68d18cd9
Juan Ortiz <juandefelix@yahoo.es>
2014-05-19 15:32:44 -0500

    added integration tests for home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,6 +10,29 @@ describe "Static Pages" do
     it { should have_content('Makeup Scheduler')}
     it { should have_title(full_title('')) }
     it { should_not have_title("| Home") }
+
+    describe "without signing in" do
+      it { should have_link("Sign in") }
+      it { should_not have_link("Available Makeups")}
+      it { should_not have_link("Notify an Absence")}
+    end
+
+    describe "after signin in" do
+      before { visit signin_path } 
+      let(:user) { FactoryGirl.create(:user) }
+      
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end     
+
+      it { should_not have_link("Sign in") }
+      it { should have_link("Available Makeups")}
+      it { should have_link("Notify an Absence")}
+    end
+
+
   end
 
   describe "Help Page" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -59,11 +59,6 @@ describe "User pages" do
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
-
-      # describe "followed by signout" do
-      #   before { click_link "Logout" }
-      #   it { should have_link('Sign in') }
-      # end
     end
   end
 end
