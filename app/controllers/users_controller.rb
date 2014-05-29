73ebb950f459b99a82fa991d0d41431160fb1ea1
Juan Ortiz <juandefelix@yahoo.es>
2014-05-29 14:02:16 -0500

    removed pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
-require 'pry'
-
 class UsersController < ApplicationController
   def new
     @user = User.new
   end
 
   def show
-    # binding.pry
+
     redirect_to root_path unless signed_in?
     @user = User.find(params[:id])
     @created = @user.created_cancellations
