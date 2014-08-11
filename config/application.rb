bc8e71f518a6039713c9b1f5b5d59c08c98efaf2
Juan Ortiz <juandefelix@yahoo.es>
2014-08-11 11:33:49 -0500

    added facebook API KEY and SECRET to ENV variables

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(:default, Rails.env)
 
 module RailsMakeupScheduler
   class Application < Rails::Application
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -15,13 +22,12 @@ module RailsMakeupScheduler
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     I18n.enforce_available_locales = true
-    config.time_zone = "Central Time (US & Canada)"
   end
 end
