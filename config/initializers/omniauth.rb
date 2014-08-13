2553e2a11eb24c0a6d73dff93fdc7a71c0f75bad
Juan Ortiz <juandefelix@yahoo.es>
2014-08-13 12:18:30 -0500

    Using a proc for managing access denied in facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  OmniAuth.config.on_failure = SessionsController.action(:failure)
+  OmniAuth.config.on_failure = Proc.new do |env|
+    SessionsController.action(:failure).call(env)
+    #this will invoke the omniauth_failure action in UsersController.
+  end
 end
