ed4126fb4f20f4bb4ac6e89719f81874ea2be0c3
Juan Ortiz <juandefelix@yahoo.es>
2014-09-09 21:24:57 -0500

    updated environment files

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module RailsMakeupScheduler
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.version = '1.0'
     I18n.enforce_available_locales = true
   end
 end
