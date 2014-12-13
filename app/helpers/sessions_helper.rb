88b0a08dd7c61bffc34e92d51689c029a5739245
juandefelix <juandefelix@yahoo.es>
2014-12-13 17:30:13 -0600

    added if current_user to helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,8 +20,10 @@ module SessionsHelper
   end
 
   def sign_out
-    current_user.update_attribute(:remember_token, User.digest(User.new_remember_token))
-    cookies.delete(:remember_token)
-    current_user = nil
+    if current_user
+      current_user.update_attribute(:remember_token, User.digest(User.new_remember_token))
+      cookies.delete(:remember_token)
+      current_user = nil
+    end
   end
 end
