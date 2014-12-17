4361f4ad55989d4dc7f91a9c8a42c2f56ea6b032
juandefelix <juandefelix@yahoo.es>
2014-12-16 21:06:55 -0600

    generated admin controllers and views

---
+++ app/controllers/admin/cancellations_controller.rb
@@ -0,0 +1,26 @@
+class Admin::CancellationsController < ApplicationController
+  def index
+  end
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+    @cancellation.update_attribute(:instrument, params[:cancellation][:instrument])
+    redirect_to cancellations_path, warning: "Successfully updated"
+  end
+
+
+  end
+
+  def index
+  end
+end
