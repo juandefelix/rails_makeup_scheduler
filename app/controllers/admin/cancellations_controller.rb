956a088335353dcab57bfb0b979ceaceef390bd4
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2015-01-12 14:14:10 -0600

    added admin index and update methods in controller

--- app/controllers/admin/cancellations_controller.rb
+++ app/controllers/admin/cancellations_controller.rb
@@ -1,5 +1,24 @@
 class Admin::CancellationsController < ApplicationController
   def index
+    @cancellations = Cancellation.all
+
+    @month = (params[:month] || (Time.zone || Time).now.month).to_i
+    @year = (params[:year] || (Time.zone || Time).now.year).to_i
+
+    @shown_month = Date.civil(@year, @month)
+    @event_strips = Cancellation.event_strips_for_month(@shown_month)
+
+    # To restrict what events are included in the result you can pass additional find options like this:
+    #
+    # @event_strips = Event.event_strips_for_month(@shown_month, :include => :some_relation, :conditions => 'some_relations.some_column = true')
+    #
+  end
+
+  def day
+    year = params[:year].to_i
+    month = params[:month].to_i
+    day = params[:day].to_i
+    @cancellations = Cancellation.by_day(year, month, day).reorder(:start_at)
   end
   
   def show
@@ -12,6 +31,7 @@ class Admin::CancellationsController < ApplicationController
   end
 
   def edit
+    @cancellation = Cancellation.find params[:id]
   end
 
   def update
