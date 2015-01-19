895df250b7f19320003e83e2fc18275fe1c7c970
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2015-01-19 15:06:32 -0600

    refactored validations

--- app/models/cancellation.rb
+++ app/models/cancellation.rb
@@ -7,14 +7,12 @@ class Cancellation < ActiveRecord::Base
 
   has_event_calendar 
 
+  validates_presence_of :name, :instrument, :start_at, :creator_id
+
   validates :start_at, format: { with: /20\d{2}[-\/][01]?\d[-\/][0-3]?\d [0-2]?\d:[0-5]?\d/i, 
                                  message: "must have the right format" }
-  validates :name, :instrument, :start_at,:creator_id, presence: true
-
-  with_options if: "start_at.present?" do |cancellation|
-    cancellation.validate :in_the_past
-    cancellation.validate :less_than_24
-  end
+  validate :in_the_past, if: "start_at.present?"
+  validate :less_than_24, if: Proc.new { |c| c.start_at.present? && !c.in_the_past? }
 
   resourcify
 
