3415179eccc90a15612a04a36bfe6137b5f9955a
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2015-01-23 12:56:18 -0600

    updated day view

--- spec/requests/admin_pages.rb
+++ spec/requests/admin_pages.rb
@@ -22,13 +22,16 @@ describe "Admin Pages" do
 
     it { should have_link('delete', href: admin_user_path(user)) }
 
+    it "should not be able to delete admin user" do 
+      should_not have_link('delete', href: user_path(admin))
+    end
+
     it "should be able to delete another user" do
       expect do
         click_link('delete', match: :first)
       end.to change(User, :count).by(-1)
     end
 
-    it { should_not have_link('delete', href: user_path(admin)) }
   end
 
   describe "Admin Calendar" do
@@ -41,6 +44,32 @@ describe "Admin Pages" do
       # save_and_open_page
       page.should have_css('h1', :text => "Admin Calendar")
       page.should_not have_css('h1', :text => "Available Makeups")
+    end
+
+    describe "Day link" do
+      describe 'empty day should not display a table' do
+        before do 
+          click_link '1'
+        end
+        it { should have_content "Cancellations for 1"}
+
+        its(:html) { should_not include('<table>') }
+        it { should have_content "Sorry, no cancellations today!"}
+      end
+
+      describe 'day with absences should display a table' do
+        before do 
+          @cancellation = FactoryGirl.create(:cancellation)
+          @cancellation.update_attribute(:start_at, Time.now.beginning_of_month)
+          click_link '1'
+        end
+
+        it { should have_content "Cancellations for 1" }
+        its(:html) { should include('table') }
+        it { should have_content @cancellation.name }
+
+        it { should_not have_content "Sorry, no cancellations today!"}
+      end
     end
   end
 end
