4a19dcb5202574d61e04cac02d91db0f16c238be
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2015-01-23 13:44:47 -0600

    extracted code and moved to admin_day_spec

--- spec/requests/admin/admin_calendar_spec.rb
+++ spec/requests/admin/admin_calendar_spec.rb
@@ -14,32 +14,7 @@ describe "Admin Calendar" do
   it "should have the right content" do
     # save_and_open_page
     page.should have_css('h1', :text => "Admin Calendar")
+    page.should have_css('.ec-calendar')
     page.should_not have_css('h1', :text => "Available Makeups")
-  end
-
-  describe "Day link" do
-    describe 'empty day should not display a table' do
-      before do 
-        click_link '1'
-      end
-      it { should have_content "Cancellations for 1"}
-
-      its(:html) { should_not include('<table>') }
-      it { should have_content "Sorry, no cancellations today!"}
-    end
-
-    describe 'day with absences should display a table' do
-      before do 
-        @cancellation = FactoryGirl.create(:cancellation)
-        @cancellation.update_attribute(:start_at, Time.now.beginning_of_month)
-        click_link '1'
-      end
-
-      it { should have_content "Cancellations for 1" }
-      its(:html) { should include('table') }
-      it { should have_content @cancellation.name }
-
-      it { should_not have_content "Sorry, no cancellations today!"}
-    end
   end
 end
