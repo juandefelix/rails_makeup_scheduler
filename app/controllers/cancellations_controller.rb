c8bfb0a21b00b6b26542efbac818ae629a291c92
juandefelix <juandefelix@yahoo.es>
2015-01-22 22:06:03 -0600

    added school code ENV variable to controller

--- app/controllers/cancellations_controller.rb
+++ app/controllers/cancellations_controller.rb
@@ -95,7 +95,7 @@ class CancellationsController < ApplicationController
     end
 
     def check_school_code
-      unless params[:school_code] == CONFIG[:school_code]
+      unless params[:school_code] == ENV['school_code']
         redirect_to new_cancellation_path, flash: { danger: 'School Code is not correct' }
       end
     end
