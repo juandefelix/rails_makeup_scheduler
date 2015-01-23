b2163f46fd0ba968442de850c073377ca11345a3
juandefelix <juandefelix@yahoo.es>
2015-01-22 22:05:33 -0600

    added school code ENV variable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,12 +27,13 @@ RailsMakeupScheduler::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  ENV["school_code"] = YAML.load(File.read(File.expand_path('../../school_code.yml', __FILE__))).values[0]
+
   config.before_configuration do
     config = YAML.load(File.read(File.expand_path('../../local_env.yml', __FILE__)))
     config.merge! config.fetch(Rails.env, {})
     config.each do |key, value|
       ENV[key] = value.to_s unless value.kind_of? Hash
     end
-    CONFIG = YAML.load(File.read(File.expand_path('../../school_code.yml', __FILE__))).symbolize_keys!
   end
 end
