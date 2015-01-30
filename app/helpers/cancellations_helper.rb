d00c88b44131f9bc85d4aae369c3081f69e319fb
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2015-01-30 15:47:23 -0600

    removed time helpers from cancellations helper

--- app/helpers/cancellations_helper.rb
+++ app/helpers/cancellations_helper.rb
@@ -68,40 +68,6 @@ module CancellationsHelper
     eval_time
   end
 
-# ======================== #
-#  time formatting helpers #
-# ======================== #
-
-  def next_date_formatted
-    next_date_available.strftime("%m/%d/%y")
-  end
-
-  def next_date_available
-    return (Time.now.beginning_of_day + 2.days) if eval_time > 23
-    Time.now.beginning_of_day + 1.day
-  end
-
-  def formatted_time_now
-    Time.now.strftime("%m/%d/%y")
-  end
-
-  def taken_time(user, index)
-    cancellation = user.taken_cancellations[index]
-    "#{cancellation.get_date} #{cancellation.get_time}" if cancellation
-  end
-
-  def current_year
-    Time.now.year
-  end
-
-  def current_month
-    Time.now.month
-  end
-
-  def current_date_url_matcher
-    "#{current_year}/#{current_month}"
-  end
-
 # ================================ #
 # custom options for this calendar #
 # ================================ #
