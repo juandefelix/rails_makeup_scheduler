beecc6b9e125f774f14b7896d2e9e27ad0f32d23
cdelmoral <carderomad@gmail.com>
2015-02-26 19:18:01 -0600

    test pull request

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ RailsMakeupScheduler::Application.routes.draw do
   end
 
   get '*fallback', to: 'static_pages#fallback'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
