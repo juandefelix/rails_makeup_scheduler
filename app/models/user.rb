15c6023c561689ee814981942f85d0d8f3851982
juandefelix <juandefelix@yahoo.es>
2015-03-21 14:18:20 -0500

    removed some validations and strategies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable, 
-         :recoverable, :validatable, :omniauthable, :confirmable
+  devise :database_authenticatable, :registerable, :rememberable, :recoverable, :validatable, :omniauthable #,  :confirmable
   rolify
   before_save { email.downcase! }
   before_create :create_remember_token
 
   default_scope -> { order(name: :desc) }
 
-  validates :name, :email, :provider, presence: true
+  validates :name, :email, presence: true
   validates :name, length: { in: 4..50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :uid, presence: true, uniqueness: { case_sensitive: false }
+  # validates :uid, presence: true, uniqueness: { case_sensitive: false }
   # validates :password, length: { minimum: 6 }
 
   belongs_to :business
