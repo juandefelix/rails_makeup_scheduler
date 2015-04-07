a083704dd9333cb46915d2ba91edc2a0d8f5bc83
juandefelix <juandefelix@yahoo.es>
2015-04-06 22:38:15 -0500

    added helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,4 +33,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
+
+  def current_user?(user)
+    current_user == user
+  end
 end
