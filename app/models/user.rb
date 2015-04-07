91e0d452af012b14e923f3d056afc25cf7576238
juandefelix <juandefelix@yahoo.es>
2015-04-06 20:29:21 -0500

    created User.from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,13 +28,10 @@ class User < ActiveRecord::Base
 
 
   def self.from_omniauth(auth)
-    binding.pry
-    email = auth[:info][:email]
-    where(email: auth[:info][:email]).first_or_create do |user|
-      user.provider ||= auth["provider"]
-      user.uid ||= auth["uid"]
-      user.name ||= auth["info"]["name"]
-      user.email ||= auth["info"]["email"]
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
     end
   end
 
