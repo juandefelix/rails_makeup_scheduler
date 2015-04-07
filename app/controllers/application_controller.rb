3321fc14210807f059708476b9716bbedc678fb0
juandefelix <juandefelix@yahoo.es>
2015-04-06 22:49:02 -0500

    removed helper from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,8 +33,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
-
-  def current_user?(user)
-    current_user == user
-  end
 end
