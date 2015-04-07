7546ccbcdebc43dd856e6505a610d402d16ccd87
juandefelix <juandefelix@yahoo.es>
2015-04-06 22:21:05 -0500

    updated users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   before_action :redirect_to_home_if_not_signed_in
-  
+
   def show
     @user = User.find(params[:id])
-    @created = @user.created_cancellations
-    @taken = @user.taken_cancellations
+    if current_user.has_role?(:admin) || current_user == @user
+      @created = @user.created_cancellations
+      @taken = @user.taken_cancellations
+    else
+      flash[:danger] = "You can not access this page"
+      redirect_to root_path
+    end
   end
 end
