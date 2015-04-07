56761d28686ac411986b4ef4420fc430389c66db
juandefelix <juandefelix@yahoo.es>
2015-04-06 21:44:45 -0500

    updated users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,9 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
+  before_action :redirect_to_home_if_not_signed_in
+  
   def show
-    redirect_to root_path unless signed_in?
     @user = User.find(params[:id])
     @created = @user.created_cancellations
     @taken = @user.taken_cancellations
   end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Makeup Scheduler!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
 end
