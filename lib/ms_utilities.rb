ff4f3d5430fe01957c5031ed059594ace66dd655
juandefelix <juandefelix@yahoo.es>
2015-05-03 16:33:01 -0500

    updated email regex

--- lib/ms_utilities.rb
+++ lib/ms_utilities.rb
@@ -1,4 +1,4 @@
 module MsUtilities
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-zA-Z\d\-]+(?:\.[\w\-]+)*\.[\w]+\z/
   VALID_DATE_REGEX = /20\d{2}[-\/][01]?\d[-\/][0-3]?\d [0-2]?\d:[0-5]?\d/i
 end
