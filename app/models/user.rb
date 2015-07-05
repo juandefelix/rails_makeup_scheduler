ae69e75c3be46e014c3483912d21c11f9b8089a2
juandefelix <juandefelix@yahoo.es>
2015-07-05 12:34:02 -0500

    devise registration and confirmation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  # :confirmable, :lockable, :timeoutable and 
+  devise :registerable, :omniauthable, :recoverable, 
+         :rememberable, :trackable,    :validatable, 
+         :confirmable,  :database_authenticatable 
   rolify
   before_save { email.downcase! }
   before_create :create_remember_token
 
-  validates :name, :email, :provider, presence: true
-  validates :name, length: { in: 4..50 }
+  # validates :name, :email, :provider, presence: true
+  # validates :name, length: { in: 4..50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :uid, presence: true, uniqueness: { case_sensitive: false }
+  # validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  # validates :uid, presence: true, uniqueness: { case_sensitive: false }
   # validates :password, length: { minimum: 6 }
 
   # has_secure_password
