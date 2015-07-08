0b3fc4f7df4623acb3717aa7e4bdb678bb0b83c1
Juan Ortiz <juandefelix@yahoo.es>
2015-07-08 16:52:42 -0500

    Update omniauth_callbacks_controller.rb

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    @user.skip_confirmation!
+    # @user.skip_confirmation!
     
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
